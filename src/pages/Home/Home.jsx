5ee70c80c518808a81971af078fd4c6477b56c65
Asrafunnessa <ayeshapriya0@gmail.com>
2023-10-06 22:56:20 +0600

    2nd: set navbar and banner section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
+import Banner from "../Shared/Banner/Banner";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Home = () => {
     return (
         <div>
-            <h2>home</h2>
+            <Navbar></Navbar>
+            <Banner></Banner>
+            <h2 className="text-2xl">home</h2>
         </div>
     );
 };
